0542a053c1daa5f0d9fc259d2a37766ffba4350f
Josiah Adesola <59745353+Josiah-Adesola@users.noreply.github.com>
2023-03-01 23:43:22 +0100

    Add files via upload

--- loops/for_loop.sh
+++ loops/for_loop.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+# Basic for loop
+names='Stan Kyle Cartman'
+for name in $names
+do
+echo $name
+done
+echo All done
 
+
+
+# for loop for number in series
+
+
+for value in {1..8}
+do
+	echo $value
+done
+
+echo All printed
